42abf5f2cdac20cb74749c8c7d3a52b8f518ec7e
LiamTec <liam.gonzales@tecsup.edu.pe>
2025-04-09 14:30:03 -0500

    creacion de los modelos, views.py y views.py

--- agenda_app/src/agenda/models.py
+++ agenda_app/src/agenda/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Contacto(models.Model):
+    nombre = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=15)
+    email = models.EmailField()
+    direccion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
--- agenda_app/src/agenda/views.py
+++ agenda_app/src/agenda/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Contacto
+from .forms import ContactoForm
 
-# Create your views here.
+def lista_contactos(request):
+    contactos = Contacto.objects.all()
+    return render(request, 'contactos/lista.html', {'contactos': contactos})
+
+def agregar_contacto(request):
+    if request.method == 'POST':
+        form = ContactoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_contactos')
+    else:
+        form = ContactoForm()
+    return render(request, 'contactos/formulario.html', {'form': form})
+
+def editar_contacto(request, pk):
+    contacto = get_object_or_404(Contacto, pk=pk)
+    if request.method == 'POST':
+        form = ContactoForm(request.POST, instance=contacto)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_contactos')
+    else:
+        form = ContactoForm(instance=contacto)
+    return render(request, 'contactos/formulario.html', {'form': form})
+
+def eliminar_contacto(request, pk):
+    contacto = get_object_or_404(Contacto, pk=pk)
+    if request.method == 'POST':
+        contacto.delete()
+        return redirect('lista_contactos')
+    return render(request, 'contactos/eliminar.html', {'contacto': contacto})
